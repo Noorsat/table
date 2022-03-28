9078a7f9351600efb485482c242aa4f4e9b32df1
Noorsat <itsnursat@gmail.com>
2022-03-29 02:16:17 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, {useState} from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 import './App.css';
-import TableSection from './components/TableSection'
-
-import { data } from './data';
+import Films from './components/Films'
 
 const App = () => {
-  const tableData = data.toptracks.track;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); 
 
-  console.log(data)
-
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=d65708ab6862fb68c7b1f70252b5d91c&language=ru-RU&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate').then(response => {
+      if (response.ok){
+        return response.json();
+      }
+      throw response;
+    }).then(data => {
+      setData(data);
+    }).finally(() => {
+      setIsLoading(true)
+    })
+  }, [])
+  
   return (
     <div className="App">
-      <TableSection data={tableData} />
-    </div>
+      {
+        isLoading ? <Films data={data.results} /> : ""
+      }
+    </div>  
   );
 }
 
